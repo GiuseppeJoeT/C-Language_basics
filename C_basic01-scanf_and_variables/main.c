3ed46758b5794a4a39efb27690db06efe62de4cf
Giuseppe Tiberi <tiberiweb@gmail.com>
2017-07-22 02:18:48 +0200

    C language basics
    C language basics, scanf and variables

---
+++ C_basic01-scanf_and_variables/main.c
@@ -0,0 +1,17 @@
+// this program can read 2 integers from keyboard and print the sum of them
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+    int A;
+    int B;
+    int sumResult;
+    
+    // scanf 1st argument --> input from keyboard
+    // scanf 2nd argument --> memory space for the variable which is getting the keyboard input
+    
+    scanf("%d", &A);
+    scanf("%d", &B);
+    printf("The sum result is %d", sumResult);
+}
